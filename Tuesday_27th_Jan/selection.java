3a2d7d1582fbcafa9c527c9a466bddefcfd7034d
paraspathania <paraspathania705@gmail.com>
2026-01-27 13:55:55 +0530

    Selection Sort

--- Tuesday_27th_Jan/selection.java
+++ Tuesday_27th_Jan/selection.java
@@ -1,5 +1,41 @@
 package Tuesday_27th_Jan;
 
+import java.util.Scanner;
+
 public class selection {
-    
+    public static void main(String[] args) {
+        Scanner d = new Scanner(System.in);
+        int n = d.nextInt();
+
+        int arr []= new int [n];
+
+        for(int i = 0; i<n; i++){
+            arr[i] = d.nextInt();
+        }
+
+        System.out.println("Array before sorting");
+        for(int i = 0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+        for(int i = 0; i < n - 1; i++){
+            int min = i;
+
+            for(int j = i; j<n; j++){
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("Array after the selection sort ");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
+
