755a2b7ab3d30f5b94218f41bd464892af3e5eb5
Paras Pathania <122259286+paraspathania@users.noreply.github.com>
2026-07-24 12:04:41 +0530

    Time: 0 ms (100%), Space: 43.7 MB (39.98%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
@@ -5,7 +5,7 @@ class Solution {
         int l = 0;
         int total = 0;
 
-        for(int i = 0; i < n; i++){            // O(n);
+        for(int i = 0; i < n; i++){            
             total += nums[i];  
         }
 
